325914bc4c31f4c59f91ceb0511a0b33f2692f47
Adeyemi Adeoye <adeyemi_adeoye@hotmail.com>
2021-05-22 13:46:52 -0500

    refactors script.js

--- script/script.js
+++ script/script.js
@@ -1,41 +1,53 @@
-const navToggler = document.getElementById('toggler');
-
-// when the collapse icon of nav is clicked
-document.addEventListener('click', e => {
-
-    if (!e.target.matches('#toggler')) return;
+const hidePicOnNavButtonClick = () => {
 
     const bioPic = document.getElementById('bio-pic');
 
     const brand = document.getElementById('brand')
-        // toggle profile pic to avoid getting in the way of nav
+
+    // toggle profile pic to avoid getting in the way of nav
     bioPic.classList.toggle('hidden')
-        // toggle title class from brand to remove left margin
+
+    // toggle title class from brand to remove left margin
     brand.classList.toggle('title')
-
-})
-
-document.addEventListener('click', e => {
-
-    if (!e.target.matches('#nav-projects')) return;
-    console.log('clicked')
-
-    e.preventDefault();
-
-    const projectsHtml = getPages('https://raw.githubusercontent.com/yemi-adeoye/yemi-adeoye.github.io/main/pages/projects.html');
-
-    projectsHtml.then(data => {
-            console.log(data)
-        }
-
-    )
-
-})
-
-// get the projects for the page
-const getPages = async(url) => {
-    const page = await fetch(url);
-    return page;
 }
 
-const navProjects = document.getElementById('nav-projects')
+const loadPage = async(url, id) => {
+    // read page contents
+    const page = await fetch(url)
+        .then(data => data.json())
+        .then((data) => {
+            const body = document.getElementById(id);
+            body.innerHTML = data.data;
+            console.log(data);
+        })
+        .catch(e => {
+            body.innerHTML = '<div>Awww snap! Something went wrong! Try reloading the page ensuring that you have an internet connection. </div>' + e;
+        });
+}
+
+const eventHandler = (eventType, origin, callback) => {
+
+    document.addEventListener(eventType, e => {
+
+        if (!e.target.matches(origin)) return;
+
+        e.preventDefault();
+
+        callback();
+
+    })
+}
+
+
+/* global code goes here */
+const navToggler = document.getElementById('toggler');
+const navProjects = document.getElementById('nav-projects');
+
+// when the projects link is clicked
+eventHandler('click', '#nav-projects', () => { loadPage('https://raw.githubusercontent.com/yemi-adeoye/yemi-adeoye.github.io/main/pages/projects.html', 'body') })
+eventHandler('click', '#toggler', hidePicOnNavButtonClick);
+
+document.onload(
+    eventHandler('click', '#nav-projects', () => { loadPage('https://raw.githubusercontent.com/yemi-adeoye/yemi-adeoye.github.io/main/pages/projects.html', 'body') })
+
+)
