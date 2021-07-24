6a34a63662f7afba29b5ad538a1458eacbff36d0
Adeyemi Adeoye <adeyemi_adeoye@hotmail.com>
2021-07-24 18:08:40 -0500

    updates script.js

--- script/script.js
+++ script/script.js
@@ -29,7 +29,7 @@ const getProjectsHtml = (projects) => {
     }
     return `<div class="row p-5 d-block"><h1 class="main">Some Projects</h1></div>
 <div class="project-container p-5">
-${projects} </div>`;
+${projectsHtml} </div>`;
 }
 
 const loadPage = async(url, id) => {
