35e1f41af3a3e3d3cc3b374fcd24a7a1e49a1e66
Adeyemi Adeoye <adeyemi_adeoye@hotmail.com>
2021-07-24 19:14:25 -0500

    mu

--- script/script.js
+++ script/script.js
@@ -12,14 +12,15 @@ const hidePicOnNavButtonClick = () => {
 }
 
 const getProjectHtml = (project) => {
-    return `<div class="row">
+    return `<div>${project.title}</div>`
+        /*return `<div class="row">
     <h2 class="project-header">${project.title}</h2>
     <a href="${project.href}" class="d-block"><img src="${project.img}" class="img project-image d-block" alt=""/></a>
     <p class="project-desc">
     ${project.desc}
       <a href="${project.href}">here</a> for more.
     </p>
-  </div>`;
+  </div>`;*/
 }
 
 const getProjectsHtml = (projects) => {
