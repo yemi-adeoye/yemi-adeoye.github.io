63467bb2ad368fdb56d1f98a557786ce074d2fe2
Adeyemi Adeoye <adeyemi_adeoye@hotmail.com>
2021-07-24 19:34:10 -0500

    mu

--- script/script.js
+++ script/script.js
@@ -84,7 +84,7 @@ const navToggler = document.getElementById('toggler');
 const navProjects = document.getElementById('nav-projects');
 
 // when the projects link is clicked
-eventHandler('click', '#nav-projects', () => { loadProjects('https://raw.githubusercontent.com/yemi-adeoye/yemi-adeoye.github.io/refactor/pages/projects.json', 'body') })
+eventHandler('click', '#nav-projects', () => { loadProjects('https://raw.githubusercontent.com/yemi-adeoye/yemi-adeoye.github.io/main/pages/projects.json', 'body') })
 eventHandler('click', '#home', () => { loadPage('https://raw.githubusercontent.com/yemi-adeoye/yemi-adeoye.github.io/main/pages/home.json', 'body') })
 eventHandler('click', '#toggler', hidePicOnNavButtonClick);
 
